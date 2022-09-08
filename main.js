67dea3fe8aea22c986cee237d5bf164017402c1c
Šarūnas <107551364+sBACKcEND@users.noreply.github.com>
2022-09-08 03:54:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ $(document).ready(function () {
 
     $.get(apiURL, function (data) {
       let { name } = data;
+      let date = new Date();
       document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + name + "')";
       let { icon } = data.weather[0];
@@ -26,12 +27,50 @@ $(document).ready(function () {
       $("#weather_description").html(data.weather[0].description);
       $("#wind").html("Wind speed: " + data.wind.speed + "km/h");
       $("#humidity").html("Humidity: " + data.main.humidity + "%");
+      $("#date").html(date.toDateString());
+
+      function currentTime() {
+        let today = new Date(); /* creating object of Date class */
+        let hour = today.getHours();
+        let min = today.getMinutes();
+        let sec = today.getSeconds();
+        let midday = "AM";
+        midday = hour >= 12 ? "PM" : "AM"; /* assigning AM/PM */
+        hour =
+          hour == 0
+            ? 12
+            : hour > 12
+            ? hour - 12
+            : hour; /* assigning hour in 12-hour format */
+        hour = updateTime(hour);
+        min = updateTime(min);
+        sec = updateTime(sec);
+        document.getElementById("today").innerText =
+          hour +
+          ":" +
+          min +
+          ":" +
+          sec +
+          " " +
+          midday; /* adding time to the div */
+        let t = setTimeout(currentTime, 1000); /* setting timer */
+      }
+
+      function updateTime(k) {
+        /* appending 0 before time elements if less than 10 */ if (k < 10) {
+          return "0" + k;
+        } else {
+          return k;
+        }
+      }
+      currentTime();
+
     }).catch(function () {
       Swal.fire({
-        icon: 'info',
-        title: 'Oops...',
-        text: 'Please provide a valid city!',
-      })
+        icon: "info",
+        title: "Oops...",
+        text: "Please provide a valid city!",
+      });
     });
   }
 });
